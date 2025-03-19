1f8c6d726afabf016ef64e15faa490a7a220766e
mk9307888@gmail.com <mk9307888@gmail.com>
2025-03-19 14:16:50 -0400

    try to add into GSC

--- src/seo.tsx
+++ src/seo.tsx
@@ -18,8 +18,9 @@ const SEO: React.FC<SEOProps> = ({ title, description, image = "/default-image.p
       <meta property="og:url" content={url} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="keywords" content="Manish Kumal, Web Developer, Portfolio, Future Nurse, MERN Stack, Healthcare IT, Personal Website, Protfolio example" />
-      
       <meta name="author" content="Manish Kumal" />
+      <meta name="robots" content="index, follow"/>
+      <meta name="google-site-verification" content="PfGwE5ibySezT2WGVJ92rOZTMLiyCsAjjWa7NKgH5Cw" />
       <link rel="canonical" href={url} />
       <link rel="icon" type="image/png" href="/manish.png" />
     </Helmet>
