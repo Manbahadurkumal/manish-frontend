fd83ac9e7b0328124a9d9fc55672f563dd014063
mk9307888@gmail.com <mk9307888@gmail.com>
2025-04-09 14:23:29 -0400

    dont know

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,12 @@
+
+import { ChatMessageListDemo } from "./chat";
 import { FaqSectionDemo } from "./faq";
 
 
 const About = () =>{
     return(<>
     <FaqSectionDemo />
+    <ChatMessageListDemo />
     </>)
 }
 export default About
